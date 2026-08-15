0d823b6f17c55e17162a7fb99f6be5601309558d
sam-1388 <sam200x.sy@gmail.com>
2026-08-15 16:07:17 +0300

    can filter books & employees

--- frontned/src/pages/home/HomePageEnhanced.jsx
+++ frontned/src/pages/home/HomePageEnhanced.jsx
@@ -1,14 +1,18 @@
-import { useState ,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { Products } from './Products'
 import { Button } from '../../components/Button'
+import { BookStatus } from '../../lib/BookStatus'
 import plusIcon from '../../assets/images/icons/add.png'
+
 import api from '../../lib/axios'
 import './HomePage.css'
+
 
 export function HomePageEnhanced() {
 
   const [books, setBooks] = useState([]);
+  const [statusUpdate, setStatusUpdate] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,16 +21,17 @@ export function HomePageEnhanced() {
       setBooks(response.data.books);
     }
     getbooks();
-  }, []);
+  }, [statusUpdate]);
 
 
 
   return (
     <div className='content-container'>
       <div className='products-container'>
-        <Products
-          books={books}
-        />
+        
+        <BookStatus value={[statusUpdate, setStatusUpdate]}>
+          <Products books={books} />
+        </BookStatus>
       </div>
       <div className='add-books-container'>
         <Button
--- frontned/src/pages/home/Product.jsx
+++ frontned/src/pages/home/Product.jsx
@@ -1,64 +1,8 @@
 import { BookImage } from '../../components/BookImage';
 import { useNavigate } from 'react-router'
+import { Status } from '../../components/BookStatus';
 import './Product.css'
-import { useRef, useState } from 'react';
 
-
-function Status({ statusValue, id }) {
-  const listRef = useRef(null);
-  function handleClick(e) {
-    e.stopPropagation()
-    listRef.current.
-
-  }
-  function getColor(statusValue) {
-    switch (statusValue) {
-
-
-      case 'need translation':
-        return "#808080"
-
-      case 'need copyEditing':
-        return "#0da00d"
-
-      case 'need typeSetting':
-        return "#1515c8"
-
-      case 'need proofReading':
-        return "#c814c8"
-      case 'ready for printing':
-        return "#c3c314"
-    }
-
-  }
-  return (
-    <>
-      <style>
-        {`
-          .status-val{ 
-          font-weight :bold;
-            color : ${getColor(statusValue)} ;
-            background-color:${getColor(statusValue)}5e;
-            padding : 1px 5px;
-            border-radius:10px;
-            border-bottom:3px solid ;
-            margin-left :4px
-            
-        `}
-      </style>
-      <span className='status-val'
-        onClick={handleClick}
-      >
-        {statusValue}
-      </span>
-      <StatusSelector
-        ref={listRef}
-        statusValue={statusValue}
-        id={id}
-      />
-    </>
-  )
-}
 export function Product({ book }) {
 
   const navigate = useNavigate();
@@ -89,27 +33,3 @@ export function Product({ book }) {
   )
 }
 
-function StatusSelector({ id, statusValue, ref }) {
-
-  const [open, setOpen] = useState(false);
-  const [selectedState, setSelectedState] = useState();
-  return (
-    open && (
-      <div ref={ref}>
-        <label>
-          change the state to:
-          <select
-            value={selectedState}
-            onChange={e => setSelectedState(e.target.value)}
-          >
-            <option value="need translation"></option>
-            <option value="need copyediting"></option>
-            <option value="need typesetting"></option>
-            <option value="need proofReading"></option>
-            <option value="ready for printing"></option>
-          </select>
-        </label>
-      </div>
-    )
-  )
-}
