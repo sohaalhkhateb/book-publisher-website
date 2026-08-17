b9f3375dae1c6dfea018141423c4327d53b7c035
sam-1388 <sam200x.sy@gmail.com>
2026-08-17 16:44:48 +0300

    step1234

--- frontned/src/pages/orders/Step2.jsx
+++ frontned/src/pages/orders/Step2.jsx
@@ -1,6 +1,107 @@
-export function Step2 () {
-    return (
-        <>
-        </>
-    )
+import InputFieldWithErrors from "../../components/InputFieldWithErrors";
+import { Button } from "../../components/Button";
+import { useState } from "react";
+import upwardsArrow from '../../assets/images/icons/upwardsArrow.png'
+import closeImage from '../../assets/images/icons/close.png'
+import api from "../../lib/axios";
+import { useNavigate } from "react-router";
+
+
+export function Step2() {
+
+
+  const [email, setEmail] = useState('');
+  const [contacts, setContacts] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+
+
+  const [error, setError] = useState({});
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  async function next() {
+
+    setLoading(true);
+    setError({});
+    await api.post('/orders?step=2', {
+      email,
+      contacts,
+      phone_number: phoneNumber,
+      address,
+    }).then((response) => {
+      if (response.data.success) {
+        setLoading(false)
+        navigate('/orders/add/3')
+      }
+    }).catch((errors) => {
+      setError(errors.response.data.errors ?? errors.response.data)
+      setLoading(false)
+    })
+  }
+
+
+  return (
+    <>
+      <h2>how can we contact you ?</h2>
+
+
+      <InputFieldWithErrors
+        type='email'
+        name='email'
+        value={email}
+        setValue={setEmail}
+        error={error.email}
+        required={false}
+      />
+
+      <InputFieldWithErrors
+        type='text'
+        name='phone number'
+        value={phoneNumber}
+        setValue={setPhoneNumber}
+        error={error.phone_number}
+        required={false}
+      />
+
+      <InputFieldWithErrors
+        type='text'
+        name='address'
+        value={address}
+        setValue={setAddress}
+        error={error.address}
+        required={false}
+      />
+
+      <InputFieldWithErrors
+        type='text'
+        name='contacts'
+        value={contacts}
+        setValue={setContacts}
+        error={error.contacts}
+        required={false}
+      />
+
+      <hr/>
+
+      <Button
+        color='firebrick'
+        text='cancel'
+        position="left"
+        image={closeImage}
+        onClick={() => navigate('/orders/add/1')}
+        isLoading={loading}
+      />
+      <Button
+        color='darkgreen'
+        text='continue'
+        image={upwardsArrow}
+        onClick={next}
+        isLoading={loading}
+      />
+
+    </>
+  )
 }
--- frontned/src/pages/orders/Step4.jsx
+++ frontned/src/pages/orders/Step4.jsx
@@ -1,6 +1,80 @@
-export function Step4 () {
+import { Button } from "../../components/Button";
+import { useState } from "react";
+import upwardsArrow from '../../assets/images/icons/upwardsArrow.png'
+import closeImage from '../../assets/images/icons/close.png'
+import api from "../../lib/axios";
+import { useNavigate } from "react-router";
+
+
+export function Step4() {
+
+    const [purchase, setPurchase] = useState(null);
+    const [error, setError] = useState({});
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
+
+    async function next() {
+        setLoading(true);
+        await api.post('/orders?step=3', {
+            purchase,
+        }).then((response) => {
+            if (response.data.success) {
+                setLoading(false)
+                if (purchase) {
+                    navigate('/orders/add/5')
+                } else {
+                    navigate('/orders/add/6')
+                }
+            }
+        }).catch((errors) => {
+            setError(errors.response.data)
+            setLoading(false)
+        })
+    }
+
+
     return (
         <>
+            <h2>what would you like your order to be ?</h2>
+
+
+            i want to purchase books
+            <input
+                type='radio'
+                name='order-type'
+                checked={purchase === true}
+                onChange={() => setPurchase(true)}
+            />
+            <hr/>
+            i want publisher house services
+            <input
+                type='radio'
+                name='order-type'
+                checked={purchase === false}
+                onChange={() => setPurchase(false)}
+            />
+
+            {error.purchase && <p>{error.purchase}</p>}
+
+            <hr />
+
+            <Button
+                color='firebrick'
+                text='cancel'
+                position="left"
+                image={closeImage}
+                onClick={() => navigate('/orders/add/3')}
+                isLoading={loading}
+            />
+
+            <Button
+                color='darkgreen'
+                text='continue'
+                image={upwardsArrow}
+                onClick={next}
+                isLoading={loading}
+            />
         </>
     )
 }
