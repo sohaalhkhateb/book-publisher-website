5c53c503faa7f2cd39817bafb59a550d37e47f2d
soha_alkhateeb <sohaalkhateb8@gmail.com>
2026-08-17 19:10:46 +0300

    ..

--- frontned/src/pages/orders/Step3.jsx
+++ frontned/src/pages/orders/Step3.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react"
 import { Button } from "../../components/Button"
 import api from "../../lib/axios";
 import { useNavigate } from "react-router";
+import './Step3.css'
+import noItemsIcon from '../../assets/images/icons/no-items.png'
+import leftArrow from '../../assets/images/icons/leftArrow.png'
+import check from '../../assets/images/icons/check.png'
+import add from '../../assets/images/icons/add-white.png'
 
 export function Step3() {
   const [items, setItems] = useState([]);
@@ -12,10 +17,24 @@ export function Step3() {
         setItems(response.data)
       })
   }, [])
-
+  console.log(items)
   return (
     <>
-      <p>{items.isEmpty?null:'no items , try adding one to your order !'}</p>
+      {items.length == 0 &&
+        <div
+          className="no-items-div"
+        >
+          <img
+            src={noItemsIcon}
+            alt=""
+            style={{
+              width: '90px'
+            }}
+          />
+          <p >no items , try adding one to your order !</p>
+        </div>
+      }
+
       {
         items.map((item) => {
           return (
@@ -23,14 +42,28 @@ export function Step3() {
           )
         })
       }
-      <Button
-        text='add more order items'
-        onClick={() => navigate('/orders/add/4')}
-      />
-      <Button
-        text='submit order'
-        onClick={() => navigate('/orders/add/7')}
-      />
+      <div className="step3-btns">
+        <Button
+          position='left'
+          text='back'
+          color='var(--accent)'
+          onClick={() => navigate('/orders/add/2')}
+          image={leftArrow}
+        />
+        <Button
+          position='right'
+          text='add more order items'
+          onClick={() => navigate('/orders/add/4')}
+          image={add}
+        />
+        <Button
+          position='right'
+          text='submit order'
+          color='var(--success)'
+          onClick={() => navigate('/orders/add/7')}
+          image={check}
+        />
+      </div>
     </>
   )
 }
